67b3659eae0463540d16aa8d070293a5093d1d5d
ecoen66 <ecoen@yahoo.com>
2020-04-28 15:55:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# homebridge-comed-hourlybilling
+A [ComEd](https://www.comed.com/Pages/default.aspx) Hourly Billing plugin for
+[Homebridge](https://github.com/nfarina/homebridge).  This creates a Light Sensor in homekit,
+ where the LUX reading is actually the current hour average price.
+
+This code is heavily based on the work of Stog's [homebridge-fronius-inverter](https://github.com/Stog/homebridge-fronius-inverter) accessory.
+
+# Installation
+Run these commands:
+
+    % sudo npm install -g homebridge
+    % sudo npm install -g homebridge-comed-hourlybilling
+
+
+NB: If you install homebridge like this:
+
+    sudo npm install -g --unsafe-perm homebridge
+
+Then all subsequent installations must be like this:
+
+    sudo npm install -g --unsafe-perm homebridge-comed-hourlybilling
+
+# Configuration
+
+Example accessory config (needs to be added to the homebridge config.json):
+ ...
+
+		"accessories": [
+        	{
+				"name": "ComEd Rate",
+				"manufacturer": "ComEd",
+				"model": "Hourly Billing"
+				"accessory": "ComEd Hourly Billing"
+        	}
+      	]
+ ...
+
+### Config Explanation:
+
+Field           			| Description
+----------------------------|------------
+**accessory**   			| (required) Must always be "ComEd Hourly Billing".
+**name**					| (required) The name you want to use for for the power level widget.
+**manufacturer**			| (optional) This shows up in the homekit accessory Characteristics.
+**model**					| (optional) This shows up in the homekit accessory Characteristics.
---
+++ index.js
@@ -0,0 +1,100 @@
+const axios = require('axios');
+const setupCache = require('axios-cache-adapter').setupCache;
+
+var Service, Characteristic;
+
+const DEF_MIN_LUX = 0,
+      DEF_MAX_LUX = 10000;
+
+const PLUGIN_NAME   = 'homebridge-comed-hourlybilling';
+const ACCESSORY_NAME = 'ComEd Hourly Billing';
+
+module.exports = function(homebridge) {
+    Service = homebridge.hap.Service;
+    Characteristic = homebridge.hap.Characteristic;
+    homebridge.registerAccessory(PLUGIN_NAME, ACCESSORY_NAME, HourlyBilling);
+}
+
+/**
+ * Setup Cache For Axios to prevent additional requests
+ */
+const cache = setupCache({
+  maxAge: 5 * 1000 //in ms
+})
+
+const api = axios.create({
+  adapter: cache.adapter
+})
+
+/**
+ * Main API request with site overview data
+ *
+ * @param {siteID} the SolarEdge Site ID to be queried
+ * @param {apiKey} the SolarEdge monitoring API Key for access to the Site
+ */
+const getHourlyData = async() => {
+	try {
+	    return await api.get('https://hourlypricing.comed.com/api?type=currenthouraverage'')
+	} catch (error) {
+	    console.error(error)
+	}
+}
+
+/**
+ * Gets and returns the accessory's value in the correct format.
+ *
+ * @param {siteID} the SolarEdge Site ID to be queried
+ * @param {apiKey} the SolarEdge monitoring API Key for access to the Site
+ * @param (log) access to the homebridge logfile
+ * @return {bool} the value for the accessory
+ */
+const getHourlyValue = async (log) => {
+
+	// To Do: Need to handle if no connection
+	const hourlyData = await getHourlyData()
+
+	if(hourlyData) {
+		log.info('Data from API', hourlyData.price);
+		if (hourlyData.price == null) {
+			return 0
+		} else {
+			// Return positive value
+			return Math.abs(Math.round(hourlyData.price, 1))
+		}
+	} else {
+		// No response hourlyData return 0
+		return 0
+	}
+}
+
+class HourlyBilling {
+    constructor(log, config) {
+    	this.log = log
+    	this.config = config
+
+    	this.service = new Service.LightSensor(this.config.name)
+
+    	this.name = config["name"];
+    	this.manufacturer = config["manufacturer"] || "ComEd";
+	    this.model = config["model"] || "Hourly Billing";
+	    this.minLux = config["min_lux"] || DEF_MIN_LUX;
+    	this.maxLux = config["max_lux"] || DEF_MAX_LUX;
+    }
+
+    getServices () {
+    	const informationService = new Service.AccessoryInformation()
+        .setCharacteristic(Characteristic.Manufacturer, this.manufacturer)
+        .setCharacteristic(Characteristic.Model, this.model)
+
+        this.service.getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+	      .on('get', this.getOnCharacteristicHandler.bind(this))
+
+	    return [informationService, this.service]
+    }
+
+    async getOnCharacteristicHandler (callback) {
+	    this.log(`calling getOnCharacteristicHandler`, await getHourlyValue(this.log))
+
+	    callback(null, await getHourlyValue(this.log))
+	}
+}
